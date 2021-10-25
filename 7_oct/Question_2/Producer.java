f0ff037c10bfdd6321fb372c7fcc133fdbac1e9d
kveerwani3 <61306934+kveerwani3@users.noreply.github.com>
2021-10-25 12:36:00 +0530

    Add files via upload

--- 7_oct/Question_2/Producer.java
+++ 7_oct/Question_2/Producer.java
@@ -2,25 +2,30 @@ package 7_Oct;
 
 import java.util.concurrent.BlockingQueue;
 
-public class Consumer implements Runnable {
+public class Producer implements Runnable {
 
 	private BlockingQueue<Integer> queue;
 	
-	public Consumer(BlockingQueue<Integer> queue) {
+	
+	
+	public Producer(BlockingQueue<Integer> queue) {
 		super();
 		this.queue = queue;
 	}
 
 	@Override
 	public void run() {
+		int i=1;
 		while(true) {
 			try {
-				Integer i = queue.take();
-				System.out.println(Thread.currentThread().getName()+" picked : "+i);
+				queue.put(i);
+				System.out.println(Thread.currentThread().getName()+" added : "+i);
 				Thread.sleep(1000);
+				i++;
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
+			
 		}
 	}
 
