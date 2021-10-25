f0e2c18ee348a2a246bdedb0e24c30e8aebd7d90
kveerwani3 <61306934+kveerwani3@users.noreply.github.com>
2021-10-25 14:02:36 +0530

    Add files via upload

--- 14_oct/Question_2/NumberofYears.java
+++ 14_oct/Question_2/NumberofYears.java
@@ -1,1 +1,32 @@
+package 14_Oct;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class NumberofYears
+{
+	public static void main(String[] args) throws InterruptedException {
+		try(Scanner sc= new Scanner(System.in)){
+			while(true) {
+				try {
+					System.out.print("Enter Date (DD-MM-YYYY): ");
+					String date = sc.next();
+					LocalDate pastDate = LocalDate.parse(date,DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+					LocalDate now = LocalDate.now();
+					Period period = Period.between(pastDate, now);
+					date = pastDate.getDayOfMonth()+" "+pastDate.getMonth()+", "+pastDate.getYear();
+					String todayDate = now.getDayOfMonth()+" "+now.getMonth()+", "+now.getYear();
+					System.out.println("Number of years between "+date+" and "+todayDate+" : "+period.getYears());
+					break;
+				}
+				catch(Exception e) {
+					System.err.println("=== Error in date format you entered ===");
+					Thread.sleep(1000);
+					System.out.println("*** Try Again ****");
+				}
+			}
+		}
+	}
+}	
