ca5f477f5c84ed70c7de1199a629e01b7943c0db
kveerwani3 <61306934+kveerwani3@users.noreply.github.com>
2021-11-09 00:48:21 +0530

    Add files via upload

--- 28_oct/helper/DBConnectionFactory.java
+++ 28_oct/helper/DBConnectionFactory.java
@@ -1,1 +1,29 @@
+package 28_oct;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class DBConnectionFactory {
+
+	public static Connection getConnectionInstance() 
+	{
+		ResourceBundle bundle = ResourceBundle.getBundle("db");
+		String jdbcDriver = bundle.getString("driver");
+		String url = bundle.getString("url");
+		String user = bundle.getString("username");
+		String password = bundle.getString("password");
+		Connection con=null;
+		try {
+			Class.forName(jdbcDriver);
+			con = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			System.err.println(e.getMessage());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+	
+}
